beeb96c47465646bad8a43b1d6fecfd66167ad39
farukayaz <frkyz07.13@gmail.com>
2023-07-22 14:06:01 +0100

    - adding global.properties file

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -6,22 +6,29 @@ import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.Properties;
 
 public class Utils {
     RequestSpecification req;
 
-    public RequestSpecification requestSpecification() throws FileNotFoundException {
+    public RequestSpecification requestSpecification() throws IOException {
 
         // request builder build
         PrintStream log = new PrintStream(new FileOutputStream("logging.text"));
-        req= new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+        req= new RequestSpecBuilder().setBaseUri(getGlobalValue("baseUrl"))
                 .addQueryParam("key","qaclick123")
                 .addFilter(RequestLoggingFilter.logRequestTo(log))
                 .addFilter(ResponseLoggingFilter.logResponseTo(log))
                 .setContentType(ContentType.JSON).build();
          return req;
+    }
+    public String getGlobalValue(String key) throws IOException {
+
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream("/Users/farukayaz/Documents/RestAssuredFramework/src/test/java/resources/global.properties");
+        prop.load(fis);
+        return prop.getProperty(key);
+
     }
 }
