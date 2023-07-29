33b1a57d19f6701c1c828e6d4e34bb8e7c7f2276
farukayaz <frkyz07.13@gmail.com>
2023-07-29 14:41:29 +0100

    - cucumber html report implementation

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -5,8 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features",
-        glue = {"stepDefinitions"}, tags= "@DeletePlace")
+@CucumberOptions(features = "src/test/java/features",plugin = "json:target/jsonReports/cucumber-report.json",
+        glue = {"stepDefinitions"}, tags= "")
 public class TestRunner {
 
 }
