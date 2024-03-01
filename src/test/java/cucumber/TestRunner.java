52ad1d17a830a7c0773f75e46b3ecfaa3cb01cda
farukayaz <frkyz07.13@gmail.com>
2024-03-01 11:13:01 +0000

    - getting ready for the jenkins

--- src/test/java/cucumber/TestRunner.java
+++ src/test/java/cucumber/TestRunner.java
@@ -1,11 +1,11 @@
-package cucumber.Options;
+package cucumber;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features",plugin = "json:target/jsonReports/cucumber-report.json",
+@CucumberOptions(features = "src/test/features",plugin = "json:target/jsonReports/cucumber-report.json",
         glue = {"stepDefinitions"}, tags= "")
 public class TestRunner {
 
